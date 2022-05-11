ec65cb9dc9b5a2251916c1feb3bfb3e3d79bc4fd
raphael-jouffreau <rjdaize@gmail.com>
2022-05-11 13:23:05 +0200

    env test revert

--- app/model/dbClient.js
+++ app/model/dbClient.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const client = new Pool({
-    host: ENV['DB_HOST'],
-    user: ENV['DB_USER'],
-    password: ENV['DB_PASSWORD'],
-    database: ENV['DB_DATABASE'],
+    host: 'ec2-63-35-156-160.eu-west-1.compute.amazonaws.com',
+    user: 'tjifvrwyojabeo',
+    password: '90bdfe3501a52c52bf47f0fe29e0aafa3c45a87ac31a4f46b8ae6b0af2ee6cb2',
+    database: 'd4233tl1bkvod2',
     ssl: {
         rejectUnauthorized: false,
       },
