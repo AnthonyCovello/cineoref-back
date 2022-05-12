dd38835e5e9c78c4262a575afc9234e4ac561ed4
raphael-jouffreau <rjdaize@gmail.com>
2022-05-12 22:24:24 +0200

    env heroku

--- app/model/dbClient.js
+++ app/model/dbClient.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const client = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
     ssl: {
         rejectUnauthorized: false,
       },
