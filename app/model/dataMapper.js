f6591d1b2bf056f968afe7cbef775404616e9c8f
raphael-jouffreau <rjdaize@gmail.com>
2022-05-17 17:29:45 +0200

    hotfix category id

--- app/model/dataMapper.js
+++ app/model/dataMapper.js
@@ -450,7 +450,7 @@ const datamapper = {
 
    async getRefByCategory(id) {
       const query = {
-        text : `SELECT reference.ref, show.name AS show, public.character.name AS character, artist.name AS artist, public.user.username AS user
+        text : `SELECT reference.id, reference.ref, show.name AS show, public.character.name AS character, artist.name AS artist, public.user.username AS user
         FROM public.reference
         JOIN public.show
         on reference.show_id = show.id
