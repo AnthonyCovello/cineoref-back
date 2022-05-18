7f27e60bb81b04590a9025c8af49308131a327aa
raphael-jouffreau <rjdaize@gmail.com>
2022-05-18 15:08:17 +0200

    add id in random

--- app/model/dataMapper.js
+++ app/model/dataMapper.js
@@ -513,7 +513,7 @@ const datamapper = {
 
 async getRefByRandom(){
   const query = {
-    text : `SELECT reference.ref, show.name AS show, public.character.name AS character, artist.name AS artist, public.user.username AS user
+    text : `SELECT reference.id, reference.ref, show.name AS show, public.character.name AS character, artist.name AS artist, public.user.username AS user
     FROM public.reference
     JOIN public.show
     on reference.show_id = show.id
