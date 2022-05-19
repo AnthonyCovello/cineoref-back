82e8f742541d63d560f50f89c569fc0fb98ebe5f
raphael-jouffreau <rjdaize@gmail.com>
2022-05-19 13:43:08 +0200

    JWT expire attempt 2

--- app/handlers/jwtToken.js
+++ app/handlers/jwtToken.js
@@ -22,7 +22,7 @@ const secretKey = "mange tes morts";
 const jwToken = (req, res, next) => {
     const token =
       req.body.token || req.query.token || req.headers["x-access-token"];
-  
+  console.log(token);
     if (!token) {
       return res.status(403).send("A token is required for authentication");
     }
