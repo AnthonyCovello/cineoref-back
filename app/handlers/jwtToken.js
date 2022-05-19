b31b79fc979af00b51cadaa36e919c3c7e85fde2
raphael-jouffreau <rjdaize@gmail.com>
2022-05-19 11:09:17 +0200

    JWT again 2

--- app/handlers/jwtToken.js
+++ app/handlers/jwtToken.js
@@ -3,14 +3,14 @@ const secretKey = "mange tes morts";
 
 const jwToken = (req, res, next) => {
     
-    const token = req.body.token || req.query.token || req.headers["x-access-token"];
+    const decoded = req.body.token || req.query.token || req.headers["x-access-token"];
 
     if(!token) {
         return res.status(403).send("Un token d'indentification est requis")
     }
     try {
-        const decoded = jwt.verify(token, secretKey)
-        req.user = decoded
+        const token = jwt.verify(decoded, secretKey)
+        req.user = token
     } catch (err) {
         return res.status(401).json("Token invalide")
     }
